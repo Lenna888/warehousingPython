45cbd65e39bee4b6501a42411dce45de5e58e7ee
Laura Estefania Latorre <102530707+Lenna888@users.noreply.github.com>
2024-09-19 23:37:45 -0500

    Create configMongo.py
    Configuration of Mongo Atlas connection parameters

---
+++ configMongo.py
@@ -0,0 +1,5 @@
+config = {
+    'URI':'mongodb+srv://lelp990720:B7BWgqBdQodOXRwu@atlascluster.mt0t5.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster',
+    'DATABASE':'email_apis',
+    'COLLECTION':'email_verified'
+}
